5812a1d05381cccde81ba08f24ecbcc984b5e03d
Pavel Prichodko <prichodko.p@gmail.com>
2018-10-03 12:44:10 -0700

    Add support for custom babel config:wq

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -309,16 +309,9 @@ module.exports = {
           {
             test: /\.(js|mjs|jsx|ts|tsx)$/,
             include: paths.appSrc,
-
-            loader: require.resolve('babel-loader'),
+            loader: require.resolve('./webpack/babel-loader.js'),
             options: {
-              customize: require.resolve(
-                'babel-preset-react-app/webpack-overrides'
-              ),
-              // @remove-on-eject-begin
-              babelrc: false,
               configFile: false,
-              presets: [require.resolve('babel-preset-react-app')],
               // Make sure we have a unique cache identifier, erring on the
               // side of caution.
               // We remove this when the user ejects because the default
